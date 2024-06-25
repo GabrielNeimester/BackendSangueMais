10e868a22034ca1f5cc49763db06f7c200739527
Gabriel Neimester <gneimester@hotmail.com>
2024-06-25 20:51:43 -0300

    adicionando documentação swagger a api

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import routes from './routes'
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use(cors())
-app.use(express.json()) // habilita o express para receber dados no formato json
-app.use(routes) // habilita as rotas
+app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(routes)
 app.listen(port, () => {
   console.log(`Servidor executando na porta ${port}`)
 })
